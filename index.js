20f71f947d13447e8118ddd7feb96ffb24e3aaa2
Vimal Prasath, VR <vrvprasath@gmail.com>
2021-10-13 23:36:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// traverse the tree
+// change the index of the first child & the second (swap)
+// traverse further down
+// change the index of the inner child's (swap again)
+
+(() => {
+  let myObj = {
+    node: "A",
+    children: [
+      {
+        node: "B",
+        children: [{ node: "D" }, { node: "E" }],
+      },
+      {
+        node: "C",
+        children: [{ node: "F" }, { node: "G" }],
+      },
+    ],
+  };
+
+  /* for (const key in myObj) {
+    if (key === "children") {
+      let [tempChild0, tempChild1 ] = myObj[key];
+      myObj.children[0] = tempChild1;
+      myObj.children[1] = tempChild0;
+      console.log("modified myObj", myObj);
+    }
+  } */
+
+  const fn = (currentObj) => {
+    for (const key in currentObj) {
+      if (key === "children") {
+        let [tempChild0, tempChild1] = currentObj[key];
+        currentObj.children[0] = tempChild1;
+        currentObj.children[1] = tempChild0;
+				fn(currentObj.children[0]);
+				fn(currentObj.children[1]);
+      }
+    }
+		return currentObj;
+  };
+
+	console.log(JSON.stringify(fn(myObj)));
+})();
